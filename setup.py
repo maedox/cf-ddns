af618f62d7713487ecb0f2afdbe080f79803746b
Pål Nilsen <246270+maedox@users.noreply.github.com>
2018-11-30 11:54:51 +0100

    Add Python 3.6 and 3.7 to setup.py classifiers

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     keywords="ddns cloudflare",
     install_requires=["requests", "urllib3"],
